1e47d5a75851c4b5c82c5b7b7e1a08b69ff1791c
kai <kai@192.168.0.13>
2020-06-15 13:55:31 +0900

    first commit

---
+++ src/main/java/jp/ac/hcs/S3A142/WebportalApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.S3A142;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ac/hcs/S3A142/WebportalApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.S3A142;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
