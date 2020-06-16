84e642fc27ad524661f193197df775601bde76b2
kai <kai@192.168.0.13>
2020-06-16 12:32:07 +0900

    住所検索画面

---
+++ src/main/java/jp/ac/hcs/S3A142/zipcode/ZipCodeController.java
@@ -0,0 +1,26 @@
+package jp.ac.hcs.S3A142.zipcode;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ZipCodeController {
+
+	/**
+	 * 郵便番号から住所を検索し、結果画面を表示する
+	 * @param zipcode 検索する郵便番号(ハイフン無し)
+	 * @param principal ログイン画面
+	 * @param model モデル
+	 * @return 結果画面 - 郵便番号
+	 */
+	@PostMapping("/zip")
+	public String getZipCode(@RequestParam("zipcode") String zipcode,
+			Principal principal, Model model) {
+
+		return "zipcode/zipcode";
+	}
+}
